add171a677e5cd229c93e6da332375631ae77ca1
Artem Kasperovich <artem.kas.cfc@gmail.com>
2022-09-16 20:53:05 +0300

    test with checkboxes is added

---
+++ cypress/e2e/webdriveruniversity.com/checkboxes.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe('Verify checkboxes', () => {
+    it('Check and validate checkbox', () => {
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click()
+        cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
+    });
+    it('Uncheck and validate checkbox', () => {
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click()
+        cy.get('#checkboxes > :nth-child(5) > input').uncheck().should('not.be.checked')
+    });
+    it.only('Check and validate checkboxes', () => {
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click()
+        cy.get('input[type="checkbox"]').check(["option-1","option-2","option-3","option-4"]).should('be.checked')
+    });
+});
