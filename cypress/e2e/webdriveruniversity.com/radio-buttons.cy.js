f35c7fcf10b67dca0f3ccdab10048dd7a14b1ea2
Artem Kasperovich <artem.kas.cfc@gmail.com>
2022-09-18 16:16:14 +0300

    hooks are added in several tests

--- cypress/e2e/webdriveruniversity.com/radio-buttons.cy.js
+++ cypress/e2e/webdriveruniversity.com/radio-buttons.cy.js
@@ -1,16 +1,17 @@
 /// <reference types="Cypress" />
 
 describe('Verify radio-buttons', () => {
-    it('Check specific radio-button', () => {
+    before(() => {
         cy.visit('http://www.webdriveruniversity.com/')
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click()
+    })
+
+    it('Check specific radio-button', () => {
         cy.get('[name="color"]').eq(1).check()
         cy.get('[name="color"]').first().check()
     });
 
     it('Check the states of the specific radio-buttons', () => {
-        cy.visit('http://www.webdriveruniversity.com/')
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click()
         cy.get('[value="lettuce"]').should('not.be.checked')
         cy.get('[value="pumpkin"]').should('be.checked')
         cy.get('[value="lettuce"]').check().should('be.checked')
