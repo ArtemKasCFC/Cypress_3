0d715475c6de8ef32677e1b370edc1303c146f4d
Artem Kasperovich <artem.kas.cfc@gmail.com>
2022-09-21 12:33:57 +0300

    added staging environment

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,24 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+  if (!fs.existsSync(pathToConfigFile)) {
+    console.log('File does not exists')
+  }
+  return fs.readJson(pathToConfigFile)
+}
 
 module.exports = defineConfig({
   projectId: 'w4svx7',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const file = config.env.configFile
+      return getConfigurationByFile(file)
     },
     env: {
-      homePageWDU : "http://www.webdriveruniversity.com"
+      homePageWDU: "http://www.webdriveruniversity.com"
     },
   },
   reporter: 'cypress-multi-reporters',
